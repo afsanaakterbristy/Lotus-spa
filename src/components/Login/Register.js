7f23866b4517a1170217ac3426d8c15e01142cf9
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-11-09 22:31:14 +0600

    added

--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -55,9 +55,9 @@ const Register = () => {
 
         
   }
-   if (loading) {
-        return <div className='flex justify-center items-center min-h-[60vh]'><div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-purple-500"></div></div>
-    }
+  //  if (loading) {
+  //       return <div className='flex justify-center items-center min-h-[60vh]'><div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-purple-500"></div></div>
+  //   }
 
     return (
         <div>
