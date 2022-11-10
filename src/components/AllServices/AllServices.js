40134c62e90b3998acbfa1a9cfaeb24737580d97
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-11-10 16:43:19 +0600

    added

--- src/components/AllServices/AllServices.js
+++ src/components/AllServices/AllServices.js
@@ -20,7 +20,7 @@ const AllServices = () => {
     
     
       if (spinner) {
-        return <div className='flex justify-center items-center min-h-[60vh]'><div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-purple-500"></div></div>
+        return <div className='flex justify-center items-center min-h-[60vh]'><div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-purple-800 dark:border-purple-300"></div></div>
     }
 
     return (
